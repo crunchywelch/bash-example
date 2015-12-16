f02a7b5d0d46056b79228f5f2fc32807a2cb612b
Aaron Welch <welch@packet.net>
2015-12-16 16:26:05 -0500

    having fun

--- example_script.sh
+++ example_script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function inline_image {
+  printf '\033]1338;url='"$1"';alt='"$2"'\a\n'
+}
+
 # the flag tells bash to exit if a command fails
 set -e
 
@@ -12,3 +16,5 @@ echo -e "\033[33mCongratulations!\033[0m You've successfully run your first buil
 If you need any more help, please email us at hello@buildbox.io
 
 \033[31m<3\033[0m Buildkite"
+
+inline_image 'https://media0.giphy.com/media/8Ry7iAVwKBQpG/giphy.gif' 'Rainbows'
